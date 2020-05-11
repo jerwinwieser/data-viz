ca4803600995b51b672861c37d6d3bf3874bd5ba
Dica <j2.wieser@gmail.com>
2020-05-11 20:02:47 +0200

    loaded dataset and make ready for analyses

--- data-viz.R
+++ data-viz.R
@@ -1,2 +1,19 @@
 library(tidyverse)
+library(timetk)
+
+# conver dataset to tibble class
+df <- tk_tbl(
+  data = AirPassengers,
+  preserve_index = TRUE,
+  rename_index = "index",
+  timetk_idx = FALSE,
+  silent = FALSE
+)
+
+# separate to vars month and year  
+df <- df %>% 
+  separate(
+    col = index,
+    into = c("month", "year"),
+    sep = " ")
 
